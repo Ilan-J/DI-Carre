a811665e4550c2df2b1f6ee857b2c575dc0d5618
ilan-lo <98606043+ilan-lo@users.noreply.github.com>
2022-04-05 22:31:33 +0200

    Add files via upload

--- head.php
+++ head.php
@@ -3,69 +3,73 @@ require_once ('bdd.php');
 
 ?>
 <head>
-    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="head.css">
     <link href="https://fonts.googleapis.com/css2?family=Lemonada:wght@300&family=Montserrat:wght@500&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
     <script src="script.js"></script>
 </head>
 
 <header>
-    <div id="topnav" class="topnav">
-        <a href="index.php">
-            <img src="images/Logo.png"width="52px" height="52px" alt="Logo Groupe">
-        </a>
-        <!-- Classic Menu -->
-        <nav role="navigation" id="topnav_menu">
-            <a class="topnav_link" href="jeux.php">JEUX</a>
-            <a class="topnav_link" href="forum.php">FORUM</a>
-            <a class="topnav_link" href="leaderboard.php">LEADERBOARD</a>
-            <a class="topnav_link" href="contact.php">CONTACT</a>
+    <div id="root">
+        <div id="topnav" class="topnav">
+            <a href="index.php">
+                <img src="images/Logo.png"width="52px" height="52px" alt="Logo Groupe">
+            </a>
+            <!-- Classic Menu -->
+            <nav role="navigation" id="topnav_menu">
+                <a class="topnav_link" href="jeux.php">JEUX</a>
+                <a class="topnav_link" href="forum.php">FORUM</a>
+                <a class="topnav_link" href="leaderboard.php">LEADERBOARD</a>
+                <a class="topnav_link" href="contact.php">CONTACT</a>
+                <?php
+                if (isset($_SESSION['user']['key'])){
+                    echo "<a class='topnav_link' href='profil.php'>PROFIL</a>";
+                }?>
+                <?php
+                if (isset($_SESSION['admin'])) {
+                    if ($_SESSION['admin'] === 1) {
+                        echo "<a class='topnav_link' href='admin.php'>ADMIN</a>";
+                    }
+                }
+                ?>
+            </nav>
             <?php
             if (isset($_SESSION['user']['key'])){
-                echo "<a class='topnav_link' href='profil.php'>PROFIL</a>";
-            }?>
-            <?php
-            if (isset($_SESSION['admin'])) {
-                if ($_SESSION['admin']===1) {
-                    echo "<a class='topnav_link' href='admin.php'>ADMIN</a>";
-                }
+                echo "<a href='deco.php' class='onglet'>
+                <div id='grid_connexion'>
+                    DECONNEXION
+                </div>
+            </a>";
+
+            }else {
+                echo "
+                <a href='connexion.html' class='onglet'>
+                <div id='grid_connexion'>
+                    CONNEXION
+                </div>
+            </a>
+            ";
             }
             ?>
-        </nav>
-        <?php
-        if (isset($_SESSION['user']['key'])){
-            echo "<a href='deco.php' class='onglet'>
-            <div id='grid_connexion'>
-                DECONNEXION
-            </div>
-        </a>";
 
-        }else {
-            echo "
-            <a href='connexion.html' class='onglet'>
-            <div id='grid_connexion'>
-                CONNEXION
-            </div>
-        </a>
-        ";
-        }
-        ?>
+            <a id="topnav_hamburger_icon" href="javascript:void(0);" onclick="showResponsiveMenu()">
+                <!-- Some spans to act as a hamburger -->
+                <span></span>
+                <span></span>
+                <span></span>
+            </a>
 
-        <a id="topnav_hamburger_icon" href="javascript:void(0);" onclick="showResponsiveMenu()">
-            <!-- Some spans to act as a hamburger -->
-            <span></span>
-            <span></span>
-            <span></span>
-        </a>
-
-        <!-- Responsive Menu -->
-        <nav role="navigation" id="topnav_responsive_menu">
-            <ul>
-                <li><a href="jeux.php">JEUX</a></li>
-                <li><a href="forum.php">FORUM</a></li>
-                <li><a href="leaderboard.php">LEADERBOARD</a></li>
-                <li><a href="contact.php">CONTACT</a></li>
-                <?php
+            <!-- Responsive Menu -->
+            <nav role="navigation" id="topnav_responsive_menu">
+                <ul>
+                    <li><a href="jeux.php">JEUX</a></li>
+                    <li><a href="forum.php">FORUM</a></li>
+                    <li><a href="leaderboard.php">LEADERBOARD</a></li>
+                    <li><a href="contact.php">CONTACT</a></li>
+                    <?php
                     if (isset($_SESSION['user']['key'])){
                         echo "<li><a href='profil.php'>PROFIL</a></li>";
                     }?>
@@ -75,8 +79,9 @@ require_once ('bdd.php');
                             echo "<li><a href='admin.php'>ADMIN</a></li>";
                         }
                     }
-                ?>
-            </ul>
-        </nav>
+                    ?>
+                </ul>
+            </nav>
+        </div>
     </div>
 </header>
