2a72275609124e13b16026f16875d05e10655a84
ilan <e@e.e>
2022-04-05 22:56:42 +0200

    'modifjeu'

--- sendgamescore.php
+++ sendgamescore.php
@@ -3,25 +3,31 @@ require_once('bdd.php');
 if(isset($_POST)) {
     $id = $_POST['button'];
     if($id === "doubler"){
-        if (randompiece(40)){
+        if (randompiece(50)){
             doublescore();
         }else {
             resetscore();
         }
     }
     if($id === "+5"){
-        if (randompiece(40)){
+        if (randompiece(20)){
             plus5score();
+        }else {
+            resetscore();
         }
     }
     if($id === "x3"){
-        if (randompiece(40)){
+        if (randompiece(66)){
             triplescore();
+        }else {
+            resetscore();
         }
     }
     if($id === "x100"){
-        if (randompiece(40)){
+        if (randompiece(95)){
             x100score();
+        }else {
+            resetscore();
         }
     }
     header("location:jeux.php");
